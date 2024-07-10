cd7b42c2739fcd6fa00e44fc9cdbcf628bea066f
erik <erik@localhost.localdomain>
2024-07-09 23:13:32 -0600

    streamlined no-cloud wiki managment

--- lib/pvector/wiki.rb
+++ lib/pvector/wiki.rb
@@ -5,30 +5,38 @@ module WIKI
     def initialize k
       @id = k
       @db = PStore.new("db/wiki-#{@id}.pstore");
-      wiki!
     end
     def id
       @id
     end
     def wiki!
-      w = Wikipedia.find(@id)
-      @db.transaction { |db|
-        if %[#{db[:title]}].length == 0
-          db[:title] = w.title
-          db[:text] = [w.text.split("\n")].flatten.compact.map { |e| if %[#{e}].length > 0; e; end }.compact
-          db[:summary] = [w.summary.split("\n")].flatten.compact.map { |e| if %[#{e}].length > 0; e; end }.compact
-          db[:coordinates] = w.coordinates
-          db[:links] = w.extlinks
-          db[:images] = w.image_urls
-        end
-      }
+      @db.transaction { |db| if db[:title] == nil; WIKI.wiki(@id).each_pair { |k,v| db[k] = v }; end }
     end
     def [] k
       @db.transaction { |db| db[k] }
     end
-    def []= k,v
-      @db.transaction { |db| db[k] = v }
+    def keys
+      @db.transaction { |db| db.keys }
     end
+    def transaction &b
+      @db.transaction { |db| b.call(db) }
+    end
+  end
+  def self.wiki k
+    hh = {}
+    w = Wikipedia.find(k)
+    if w.text.class != NilClass
+      hh[:title] = w.title
+      hh[:text] = [w.text.split("\n")].flatten.compact.map { |e| if %[#{e}].length > 0; e; end }.compact
+      hh[:summary] = [w.summary.split("\n")].flatten.compact.map { |e| if %[#{e}].length > 0; e; end }.compact
+      hh[:coordinates] = w.coordinates
+      hh[:links] = w.extlinks
+      hh[:images] = w.image_url
+    else
+      hh[:title] = k
+      [:text,:summary,:coordinates,:links,:images].each { |e| hh[e] = [] }
+    end
+    return hh
   end
   def self.[] k
     @@WIKI[k]
